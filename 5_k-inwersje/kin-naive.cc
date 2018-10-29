3da24ececfba46bfda37d91c891cddef680d1a00
Kamil Dubil <dubilkamil@gmail.com>
2018-10-29 18:57:59 +0100

    renamed directories, k-inwersje naive solution (44/100)

---
+++ 5_k-inwersje/kin-naive.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+
+void f() {
+    static const int MOD = 1000000000;
+    int n, k;
+
+    std::cin >> n >> k;
+
+    int a[n];
+    int t[n][k];
+
+    for (int i = 0; i < n; ++i) {
+        std::cin >> a[i];
+    }
+
+//    programowanie dynamiczne:
+//    1 <= i <= n, 1 <= j <= k
+
+//    t(i, j) = liczba j-inwersji, które zaczynają się na pozycji i
+//    wynikiem jest (\sum_{i = 1}^{n} t(i, k)) \mod 10^9
+//
+//    t(i, 1) = 1
+//    t(i, j) = (\sum_{l > i} T(l, j - 1)[a[i] > a[l]]) \mod 10^9
+
+    for (int i = 0; i < n; ++i) {
+        t[i][0] = 1;
+    }
+    for (int j = 1; j < k; ++j) {
+        for (int i = n - 1; i >= 0; --i) {
+            t[i][j] = 0;
+            for (int l = i + 1; l < n; ++l) {
+                t[i][j] += t[l][j - 1] * (a[i] > a[l] ? 1 : 0);
+                t[i][j] %= MOD;
+            }
+        }
+    }
+
+    int res = 0;
+    for (int i = 0; i < n; ++i) {
+        res += t[i][k - 1];
+        res %= MOD;
+    }
+
+    std::cout << res << std::endl;
+}
+
+int main() {
+
+    f();
+
+    return 0;
+}
+
+
+
